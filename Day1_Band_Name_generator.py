23295505c6f4c4a8d71771f03bc9e670f0d59b85
Wasif Billah Tanoy <234126451+Wasif235@users.noreply.github.com>
2025-09-24 12:33:48 +0600

    Day_1

---
+++ Day1_Band_Name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city=input("What's the name of the city you grew up in?\n")
+pet_name=input("what is your pet's name?\n")
+print(f"Your band name could be {city} {pet_name}!")

b7b15bd21fd12702ad595059223af18f565eda7f
Wasif Billah Tanoy <234126451+Wasif235@users.noreply.github.com>
2025-10-06 03:56:58 +0600

    Day_15

---
+++ OOP.py
@@ -0,0 +1,18 @@
+class user:
+   def __init__(self,user_id, username):
+      self.id=user_id
+      self.username=username
+      self.followers = 0
+      self.following=0
+   def follow(self, user):
+      user.followers+=1
+      self.following+=1   
+      
+user_1=user("23","makato")    
+user_2=user("24","makati")  
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
+ 
